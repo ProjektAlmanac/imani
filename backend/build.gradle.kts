6ebe7a09238fff5c022507df2c328cd8efa28d3b
Antar Espadas <antar.espadas@hotmail.com>
2024-07-25 09:46:51 -0600

    Agregar dependencia a mariadb

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -41,6 +41,7 @@ dependencies {
 	kapt("org.mapstruct:mapstruct-processor:$mapStructVersion")
 
 	runtimeOnly("com.h2database:h2")
+	runtimeOnly("org.mariadb.jdbc:mariadb-java-client:3.4.1")
 
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
