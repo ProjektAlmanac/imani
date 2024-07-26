6682347fd43f6a29373b97162e96180487b8142b
Cesar <88-8@libe.com.mx>
2024-07-26 01:37:04 -0600

    se agrega pwd

--- front-paciente/src/main.ts
+++ front-paciente/src/main.ts
@@ -16,6 +16,8 @@ import { AppComponent } from './app/app.component';
 import { routes } from './app/app.routes';
 import { BASE_PATH } from './generated/openapi';
 import { environment } from './environments/environment';
+import { isDevMode } from '@angular/core';
+import { provideServiceWorker } from '@angular/service-worker';
 
 bootstrapApplication(AppComponent, {
   providers: [
@@ -24,6 +26,12 @@ bootstrapApplication(AppComponent, {
     provideRouter(routes, withPreloading(PreloadAllModules)),
     provideHttpClient(),
     { provide: BASE_PATH, useValue: environment.apiUrl },
-    provideNoopAnimations(),
+    provideNoopAnimations(), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          }), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          }),
   ],
 });
